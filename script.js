50efc7eb269b30b09ad0bd807720be533afd95a8
turankutuev <160647446+turankutuev@users.noreply.github.com>
2024-11-20 10:52:16 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function showGreeting() {
     var name = document.getElementById('child-name').value;
     if (name.trim() === '') {
-        alert('Пожалуйста, введите ваше имя.');
+        alert('Атыңызды жазыңыз.');
         return;
     }
 
     // Отображение персонального поздравления
     var greetingContainer = document.getElementById('santa-greeting');
     var personalGreeting = document.getElementById('personal-greeting');
-    personalGreeting.textContent = 'Дорогой ' + name + '  «Кумтор Голд Компани» поздравляет тебя с самым волшебным праздником — Новым годом! ❄️';
+    personalGreeting.textContent = 'Кымбаттуу ' + name + ', Жаңы жыл — бул сыйкыр жана тилектер орундалуучу убакыт. Кыялдарыңыз ишке ашсын, күндөрүңүз жаңы ачылыштарга толсун, ал эми үйүңүздө ар дайым күлкү жана бакыт болсун ❄️';
     greetingContainer.style.display = 'block';
 
     // Отображение советов
@@ -17,11 +17,11 @@ function showGreeting() {
     var tipsContent = document.getElementById('tips-content');
 
     // Здесь вы можете вставить реальные советы и включить имя ребенка
-    var tipsText = '1. Укрась елку яркими игрушками и гирляндами.\n' +
-        '2. Нарисуй новогодние открытки для близких.\n' +
-        '3. Посмотри любимые новогодние мультфильмы.\n' +
-        '4. Слепи снеговика на улице.\n' +
-        '5. Загадай самое заветное желание!';
+    var tipsText = '1. Балатыны түстүү оюнчуктар жана гирляндалар менен кооздоңуз\n' +
+        '2. Жакындарыңыз үчүн жаңы жылдык куттуктоо жазып жана тартыңыз\n' +
+        '3. Сүйүктүү Жаңы жылдык Мультфильмдерин көрүңүз.\n' +
+        '4. Көчөдө кар киши жасаңыз\n' +
+        '5. Эң жашыруун тилегиңди тиле!';
 
     childNameDisplay.textContent = name;
     tipsContent.textContent = tipsText;
